0a22d359d7a473938747882cf4d31bb30670fc9b
gchervet <gchervet@kennedy.edu.ar>
2018-05-07 13:12:06 -0300

    First commit

---
+++ CanvasQueueProcessor/QueueConsumer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.IO;
+using System.Configuration;
+
+namespace CanvasQueueProcessor
+{
+    class QueueConsumerExample
+    {
+
+        string queueName = ConfigurationSettings.AppSettings.Get("queueName");
+        string userName = ConfigurationSettings.AppSettings.Get("userName");
+        string password = ConfigurationSettings.AppSettings.Get("password");
+        string hostName = ConfigurationSettings.AppSettings.Get("hostName");
+        string virtualHost = ConfigurationSettings.AppSettings.Get("virtualHost");
+        string fileToGenerate = ConfigurationSettings.AppSettings.Get("fileToGenerate");
+        int port = Convert.ToInt32(ConfigurationSettings.AppSettings.Get("port"));
+
+        public static void Main(string[] args)
+        {
+            /*
+            string[] separators = { "[", "]"};
+            string value = "Ejemplo [EJ]";
+            string[] words = value.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var word in words)
+                Console.WriteLine(word);
+            */
+
+            QueueConsumerExample qConsumer = new QueueConsumerExample();
+            //qConsumer.GetMessage();
+            qConsumer.Register();
+        }
+
+        public void Register()
+        {
+            //Setup the connection with the message broker
+
+            //////////////////
+            FileStream ostrm;
+            StreamWriter writer;
+            TextWriter oldOut = Console.Out;
+            /////////////////
+
+            ConnectionFactory factory = new ConnectionFactory();
+            IProtocol protocol = Protocols.AMQP_0_9_1;
+            factory.VirtualHost = virtualHost;
+            factory.UserName = userName;
+            factory.Password = password;
+            factory.HostName = hostName;
+            factory.Port = port;
+            factory.Protocol = protocol;
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (ch, ea) =>
+            {
+                var body = ea.Body;
+                string message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Received Message : " + message);
+
+                ////////////////////////////////////////////////////
+                try
+                {
+                    ostrm = new FileStream(fileToGenerate, FileMode.OpenOrCreate, FileAccess.Write);
+                    writer = new StreamWriter(ostrm);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Cannot open Redirect.txt for writing");
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+                Console.SetOut(writer);
+                Console.Write(message);
+                Console.WriteLine("This is a line of text");
+                Console.WriteLine("Everything written to Console.Write() or");
+                Console.WriteLine("Console.WriteLine() will be written to a file");
+                Console.SetOut(oldOut);
+                writer.Close();
+                ostrm.Close();
+                Console.WriteLine("Done");
+                /////////////////////////////////////////////////////////
+                //                        if (!File.Exists(path))
+                //                        {
+                //                            File.Create(path);
+                //                            //.WriteAllText(path, message);
+                //                        }
+                //                        else if (File.Exists(path))
+                //                        {
+                //                            tw = new StreamWriter(path);
+                //                            //File.WriteAllText(path, message);
+                //                        }
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+            String consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+
+
+        }
+    }
+}
---
+++ CanvasQueueProcessor/TopicConsumer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.IO;
+
+namespace CanvasQueueProcessor
+{
+    class TopicConsumer
+    {
+        private const string EXCHANGE_NAME = "amq.topic";
+
+        private static void pMain(string[] args)
+        {
+            string topicName = "ilumno.lms.grades.exchange";
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.VirtualHost = "/";
+            factory.UserName = "ilumno_guest";
+            factory.Password = "Ilumno2017";
+            factory.HostName = "10.70.51.31";
+            factory.Port = 5672;
+
+            IConnection conn = factory.CreateConnection();
+
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+
+                    channel.ExchangeDeclare(topicName, "topic", true, false);
+
+                    string queueName = "ilumno.lms.grades";
+
+                    channel.QueueBind(queueName, topicName, queueName);
+
+                    Console.WriteLine("Waiting for messages");
+
+                    QueueingBasicConsumer consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume(queueName, true, consumer);
+
+
+                    while (true)
+                    {
+                        BasicDeliverEventArgs e = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                        Console.WriteLine(Encoding.ASCII.GetString(e.Body));
+
+                    }
+                }
+            }
+        }
+    }
+}
